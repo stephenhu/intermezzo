38c1c2881689930dda4cc446fbf8812cf513a514
vmwarechina <hus@vmware.com>
2015-11-22 00:22:26 +0800

    fix gulpfile staging and add clean task

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,21 @@
+var del  = require("del");
 var gulp = require("gulp");
 var path = require("path");
+var debug = require("gulp-debug");
 var symlink = require("gulp-symlink");
 
 var config = require("./config/assets.json");
 
-gulp.task("default", function() {
-  return gulp.src(config.assets, {base: "node_modules",
+gulp.task("clean:staging", function() {
+  return del(config.assets, {cwd: "public"});
+});
+
+gulp.task("staging", function() {
+  return gulp.src(config.assets, {cwd: "node_modules",
     read: false})
     .pipe(symlink(function(file) {
-      console.log(file.relative);
       return path.join("public", file.relative);
     }));
 });
+
+gulp.task("default", ["staging"]);
